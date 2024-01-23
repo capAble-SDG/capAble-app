43053f1edd7a47098b0e809704233e928e760158
fauxriarty <aditya.chheda04@gmail.com>
2024-01-24 00:51:17 +0530

    feat: made login screen

--- app/src/main/java/com/example/workable/LoginActivity.kt
+++ app/src/main/java/com/example/workable/LoginActivity.kt
@@ -1,11 +1,50 @@
 package com.example.workable
 
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.text.style.ForegroundColorSpan
+import android.view.View
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val backButton: ImageButton = findViewById(R.id.back)
+        backButton.setOnClickListener {
+            startActivity(Intent(this, SplashActivity::class.java))
+        }
+
+        val textViewLoginPrompt: TextView = findViewById(R.id.textViewSignUpPrompt)
+        val spannableString = SpannableString("Don't have an account? Sign Up")
+
+        val blackSpan = ForegroundColorSpan(Color.parseColor("#010F19")) // for colored text
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                val intent = Intent(this@LoginActivity, SignUpActivity::class.java)
+                startActivity(intent)
+            }
+
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.isUnderlineText = true // if you want login underlined
+                ds.color = Color.BLACK // text color
+            }
+        }
+
+        spannableString.setSpan(blackSpan, 23, spannableString.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannableString.setSpan(clickableSpan, 23, spannableString.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        textViewLoginPrompt.text = spannableString
+        textViewLoginPrompt.movementMethod = LinkMovementMethod.getInstance()
+
+
     }
 }
