09840f13b5cbd9ccea5e0771de1b22d71358173e
fauxriarty <aditya.chheda04@gmail.com>
2024-02-18 16:40:35 +0530

    feat: added a little to profile

--- app/src/main/java/com/example/workable/ProfileActivity.kt
+++ app/src/main/java/com/example/workable/ProfileActivity.kt
@@ -1,12 +1,26 @@
 package com.example.workable
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import com.example.workable.R
 
 class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        val back: ImageButton = findViewById(R.id.back)
+        back.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+        val home: ImageButton = findViewById(R.id.briefcaseIcon)
+        home.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+
     }
 }
